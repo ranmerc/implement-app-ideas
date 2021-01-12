45b24a3dc071a89bd2ff44854961dcdb03440507
Kamran Ansari <crossthinking786@gmail.com>
2021-01-13 00:27:46 +0530

    Weather App Complete

---
+++ beginner/Weather-App/README.md
@@ -0,0 +1,23 @@
+# Weather App
+
+A Simple Mobile Weather App based on project idea from [app-ideas repo](https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Weather-App.md). Depolyed on Github Pages [here](https://deadmercury.github.io/implement-app-ideas/beginner/Weather-App/index.html).
+
+## APIs Used
+
+- [ipapi](https://ipapi.co/api/#location-of-a-specific-ip) for fetching user's city from their ip.
+- [Nominatim](https://nominatim.org/release-docs/develop/api/Search/) to resolve city name to latitude and longitude.
+- [OpenWeatherMap](https://openweathermap.org/api) API for fetching city's current and forecaste weather.
+
+## References
+
+- SVGs from [freeicons.io](https://freeicons.io/).
+
+- localStorage on [MDN](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
+
+- [toLocaleDateString](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString#using_options) and [toLocaleTimeString](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString#using_options) on MDN.
+
+- CSS filter on [MDN](https://developer.mozilla.org/en-US/docs/Web/CSS/filter) to change color of SVG.
+
+- Center HTML Input Text Field Placeholder on [StackOverflow](https://stackoverflow.com/a/15378182).
+
+- Create a Date with a set timezone without using a string representation on [StackOverflow](https://stackoverflow.com/a/54453990).
---
+++ beginner/Weather-App/index.js
@@ -0,0 +1,190 @@
+const search = document.querySelector('#city_name');
+const loader = document.querySelector('.loading');
+const key = '836cf1cc4017da9d367d3ad71a2ccafe';
+
+async function updateDOM(res, city) {
+  function toTimeString(fromDate) {
+    return fromDate
+      .toLocaleTimeString(navigator.language, {
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZone: res.timezone,
+      })
+      .toLocaleLowerCase();
+  }
+
+  console.log(res);
+
+  let today = new Date(0);
+  today.setUTCSeconds(res.current.dt);
+  document.querySelector('#city').textContent = city;
+  document.querySelector('#day').textContent = today.toLocaleDateString(
+    navigator.language,
+    {
+      weekday: 'long',
+      timeZone: res.timezone,
+    }
+  );
+  document.querySelector('#time').textContent = toTimeString(today);
+
+  document.querySelector('#weather_description').textContent =
+    res.current.weather[0].description;
+
+  document.querySelector(
+    '#weather_icon'
+  ).src = `https://openweathermap.org/img/wn/${res.current.weather[0].icon}@2x.png`;
+
+  document.querySelector(
+    '#weather_temp'
+  ).textContent = `${res.current.temp}° C`;
+
+  document.querySelector(
+    '#feels_like_temp'
+  ).textContent = `${res.current.feels_like}° C`;
+
+  document.querySelector(
+    '#humidity > .value'
+  ).textContent = `${res.current.humidity}%`;
+
+  document.querySelector('#uvi > .value').textContent = `${res.current.uvi}`;
+
+  document.querySelector(
+    '#cloud > .value'
+  ).textContent = `${res.current.clouds}%`;
+
+  let sunrise = new Date(0);
+  sunrise.setUTCSeconds(res.current.sunrise);
+  document.querySelector('#sunrise > .value').textContent = `${toTimeString(
+    sunrise
+  )}`;
+
+  let sunset = new Date(0);
+  sunset.setUTCSeconds(res.current.sunset);
+  document.querySelector('#sunset > .value').textContent = `${toTimeString(
+    sunset
+  )}`;
+
+  let forecastTile = document.querySelectorAll('.forecast_tile');
+  for (let i = 0; i < forecastTile.length; ++i) {
+    let today = new Date(0);
+    today.setUTCSeconds(res.daily[i + 1].dt);
+    forecastTile[i].querySelector('.day').textContent = today
+      .toLocaleDateString(navigator.language, {
+        weekday: 'short',
+        timeZone: res.timeZone,
+      })
+      .toLocaleUpperCase();
+
+    forecastTile[i].querySelector(
+      '.icon > img'
+    ).src = `https://openweathermap.org/img/wn/${
+      res.daily[i + 1].weather[0].icon
+    }@2x.png`;
+    forecastTile[i]
+      .querySelector('.icon > img')
+      .setAttribute('title', res.daily[i + 1].weather[0].description);
+
+    forecastTile[i].querySelector('.highest').textContent =
+      res.daily[i + 1].temp.max;
+
+    forecastTile[i].querySelector('.lowest').textContent =
+      res.daily[i + 1].temp.min;
+  }
+}
+
+async function getTemp(city) {
+  let lon = '';
+  let lat = '';
+  try {
+    let res = await fetch(
+      `https://nominatim.openstreetmap.org/search?city=${city}&format=json`,
+      {
+        mode: 'cors',
+      }
+    );
+    if (res.ok) {
+      res = await res.json();
+      if (res.length === 0) {
+        alert('Please Enter a Valid City Name 😭');
+        return;
+      }
+      lon = res[0].lon;
+      lat = res[0].lat;
+      console.log(lat, lon, city);
+      localStorage.setItem('userCity', city);
+    } else {
+      throw new Error('API Down 🤷‍♀️');
+    }
+  } catch (e) {
+    console.error(e);
+    alert('Unable to Resolve City Name 😢');
+    return;
+  }
+  try {
+    let res = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${key}`,
+      {
+        mode: 'cors',
+      }
+    );
+    if (res.ok) {
+      res = await res.json();
+      await updateDOM(res, city);
+    } else {
+      throw new Error('API Down 🤷‍♀️');
+    }
+  } catch (e) {
+    console.error(e);
+    alert('Unable to Fetch Weather 😢');
+    document.body.innerHTML = '';
+    return;
+  }
+}
+
+async function resolveCity(e) {
+  loader.style.display = 'flex';
+  let city = '';
+  if (e) {
+    if (e.target.value === '') {
+      loader.style.display = 'none';
+      return;
+    }
+    city = e.target.value;
+    e.target.value = '';
+  } else {
+    if (localStorage.getItem('userCity')) {
+      city = localStorage.getItem('userCity');
+    } else {
+      try {
+        let res = await fetch('https://ipapi.co/json/', {
+          mode: 'cors',
+        });
+        if (res.ok) {
+          res = await res.json();
+          city = res.city;
+        } else {
+          throw new Error('API Down 🤷‍♀️');
+        }
+      } catch (e) {
+        alert('Unable to Fetch Location');
+        city = 'Pune';
+        console.error(e);
+        return;
+      }
+    }
+  }
+  console.log(city);
+  await getTemp(city);
+  loader.style.display = 'none';
+}
+
+search.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.target.blur();
+    resolveCity(e);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  resolveCity();
+});
