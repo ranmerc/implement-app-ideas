c4b7b4cbcb865b3c963c985fbe536fc43bdc5f0d
Kamran Ansari <crossthinking786@gmail.com>
2021-05-17 13:34:51 +0530

    added woff/woff2; updated gftocss.js

--- fonts/README.md
+++ fonts/README.md
@@ -1,1 +1,1 @@
-All fonts are downloaded from [Google Fonts](https://fonts.google.com/).
+All fonts are downloaded from [Google Fonts](https://fonts.google.com/) with the help of [google webfont helper](https://github.com/majodev/google-webfonts-helper).
--- scripts/gftocss.js
+++ scripts/gftocss.js
@@ -1,90 +1,72 @@
 const fs = require('fs');
 
-// When no argument is provided
 if (process.argv.length === 2) {
   console.error(`Font foldernames not specified`);
-  // process.exit(0);
-  console.log('Using all folders inside fonts/');
-  const folderNames = fs
-    .readdirSync('fonts', { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-  process.argv = process.argv.concat(folderNames);
+  process.exit(1);
 }
 
-// Generates the font-face string
-const fontFaceTemplate = (fontName, ft, weight, style) => {
+const fontFolder = `fonts/${process.argv[2]}`;
+
+if (!fs.existsSync(fontFolder)) {
+  console.error(
+    `${fontFolder}\nFolder with name ${process.argv[2]} does not exists\n`
+  );
+  process.exit(1);
+}
+
+const fontName = process.argv[2].replace(/_/g, ' ');
+
+const fontFiles = fs
+  .readdirSync(fontFolder)
+  .filter((file) => /.*\.(woff|ttf)/.test(file));
+
+let fonts = {};
+
+fontFiles.forEach((fontFile) => {
+  const [fontType, extName] = fontFile.match(/(.*)\.(.*)/).slice(1);
+  if (fonts[fontType]) fonts[fontType] = fonts[fontType].concat(extName);
+  else fonts[fontType] = [].concat(extName);
+});
+
+const fontFaceTemplate = (fontName, font, weight, style, format) => {
   return `@font-face{
   font-family: '${fontName}';
-  src: url('./${ft}');
   font-weight: ${weight};
   font-style: ${style};
+  src: ${format
+    .map((f, i) => {
+      return `url('./${font}.${f}') format('${f === 'ttf' ? 'truetype' : f}')${
+        i === format.length - 1 ? ';' : ','
+      }`;
+    })
+    .join('\n       ')}
   font-display: swap;
-}`;
+}\n\n`;
 };
 
-// used to map font file name to font weight
-const fontWeightMap = {
-  Thin: 100,
-  ExtraLight: 200,
-  Light: 300,
-  Regular: 400,
-  '': 400, // for *-Italic.ttf
-  Medium: 500,
-  SemiBold: 600,
-  Bold: 700,
-  ExtraBold: 800,
-  Black: 900,
-};
+let constructedCSS = '';
 
-let failedFonts = [];
-
-for (let i = 2; i < process.argv.length; i++) {
-  // Unzipped folder for font
-  const fontFolder = `fonts/${process.argv[i]}`;
-
-  // Check if folder Exists
-  if (!fs.existsSync(fontFolder)) {
-    console.error(
-      `${fontFolder}\nFolder with name ${process.argv[i]} does not exists\n`
-    );
-    failedFonts = failedFonts.concat(process.argv[i]);
-    continue;
-    // process.exit(1);
-  }
-
-  const fontName = process.argv[i].replace(/_/g, ' ');
-  // console.log(fontFolder, fontName);
-
-  const fontTypes = fs.readdirSync(fontFolder);
-  // console.log(fontTypes);
-
-  let constructedCSS = '';
-
-  fontTypes.forEach((ft) => {
-    // All fonts downloaded from Google Fonts are ttf.
-    if (!/.*\.ttf/.test(ft)) return;
-    const style = /Italic/.test(ft) ? 'italic' : 'normal';
-    const weight = fontWeightMap[ft.match(/-(.*)\./)[1].replace('Italic', '')];
-    console.log(
-      ft,
-      style,
-      weight,
-      fontFaceTemplate(fontName, ft, style, weight)
-    );
-    constructedCSS += fontFaceTemplate(fontName, ft, weight, style) + '\n\n';
-  });
-
-  const cssFileName = `${fontFolder}/${fontName}.css`;
-
-  fs.writeFileSync(cssFileName, constructedCSS);
-}
-
-if (failedFonts.length) {
+Object.keys(fonts).forEach((font, i) => {
+  const [fontWeight, fontStyle] = (() => {
+    let match = font.match(/latin-(\d*)(\w*)/).slice(1);
+    return [
+      match[0] ? match[0] : '400',
+      match[1] === 'italic' ? match[1] : 'normal',
+    ];
+  })();
+  const fontFormats = fonts[font].sort((a, b) => b.localeCompare(a));
   console.log(
-    `\nUnable to build following fonts, check log above for further information.`
+    fontFaceTemplate(fontName, font, fontWeight, fontStyle, fontFormats)
   );
-  failedFonts.forEach((font, i) => {
-    console.log(`${i + 1}. ${font}`);
-  });
-}
+  constructedCSS += fontFaceTemplate(
+    fontName,
+    font,
+    fontWeight,
+    fontStyle,
+    fontFormats
+  );
+});
+
+const cssFileName = `${fontFolder}/${fontName}.css`;
+
+fs.writeFileSync(cssFileName, constructedCSS);
