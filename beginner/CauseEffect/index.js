f7dc6de0e7bcec113af81623f1a789508102576b
Kamran Ansari <crossthinking786@gmail.com>
2021-07-18 18:11:42 +0530

    Improved CauseEffect; turned into pwa

--- beginner/CauseEffect/index.js
+++ beginner/CauseEffect/index.js
@@ -1,16 +1,174 @@
 let people = [];
+
+let firstNames = [
+  'Aaliyah',
+  'Aaron',
+  'Abagail',
+  'Abbey',
+  'Abbie',
+  'Abbigail',
+  'Abby',
+  'Abdiel',
+  'Abdul',
+  'Percival',
+  'Percy',
+  'Perry',
+  'Pete',
+  'Peter',
+  'Petra',
+];
+
+const lastNames = [
+  'Abbott',
+  'Abernathy',
+  'Abshire',
+  'Adams',
+  'Altenwerth',
+  'Anderson',
+  'Schroeder',
+  'Schulist',
+  'Schultz',
+  'Schumm',
+  'Schuppe',
+  'Bahringer',
+  'Bailey',
+  'Balistreri',
+  'Barrows',
+  'Bartell',
+  'Bartoletti',
+  'Barton',
+  'Bashirian',
+  'Batz',
+  'Bauch',
+  'Baumbach',
+  'Bayer',
+];
+
+const countries = [
+  'Chili',
+  'China',
+  'Christmas Island',
+  'Clipperton Island',
+  'Cocos (Keeling) Islands',
+  'Colombia',
+  'Comoren (Unie)',
+  'Congo (Democratische Republiek)',
+  'Congo (Volksrepubliek)',
+  'Cook',
+  'Coral Sea Islands',
+  'Costa Rica',
+  'Cuba',
+  'Cyprus',
+  'Denemarken',
+];
+
+const cities = [
+  'Baden-Württemberg',
+  'Bayern',
+  'Berlin',
+  'Brandenburg',
+  'Bremen',
+  'Hamburg',
+  'Hessen',
+  'Mecklenburg-Vorpommern',
+  'Niedersachsen',
+  'Nordrhein-Westfalen',
+  'Rheinland-Pfalz',
+  'Saarland',
+  'Sachsen',
+  'Sachsen-Anhalt',
+  'Schleswig-Holstein',
+  'Thüringen',
+];
+
+const streets = [
+  'Green',
+  'Greens',
+  'Grove',
+  'Groves',
+  'Harbor',
+  'Harbors',
+  'Haven',
+  'Heights',
+  'Highway',
+  'Hill',
+  'Hills',
+  'Hollow',
+  'Inlet',
+  'Inlet',
+  'Island',
+  'Island',
+  'Islands',
+  'Islands',
+  'Isle',
+  'Isle',
+  'Junction',
+  'Junctions',
+  'Key',
+  'Keys',
+  'Knoll',
+  'Knolls',
+  'Lake',
+  'Lakes',
+  'Land',
+  'Landing',
+  'Lane',
+  'Light',
+  'Lights',
+  'Loaf',
+  'Lock',
+  'Locks',
+  'Locks',
+  'Lodge',
+  'Lodge',
+];
+
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomBirthday = () => {
+  return `${getRandomNumber(1, 28)}/${getRandomNumber(1, 12)}/${getRandomNumber(
+    1990,
+    2020
+  )}`;
+};
+
+const getRandomTelephone = () => {
+  return `${getRandomNumber(100, 999)}-${getRandomNumber(
+    100,
+    999
+  )}-${getRandomNumber(1000, 9999)}`;
+};
+
+const getRandomName = () => {
+  return `${firstNames[getRandomNumber(0, firstNames.length - 1)]} ${
+    lastNames[getRandomNumber(0, lastNames.length - 1)]
+  }`;
+};
+
+const getRandomCountry = () => {
+  return countries[getRandomNumber(0, countries.length - 1)];
+};
+
+const getRandomCity = () => {
+  return cities[getRandomNumber(0, cities.length - 1)];
+};
+
+const getRandomStreet = () => {
+  return `${getRandomNumber(1000, 9999)} ${
+    streets[getRandomNumber(0, streets.length - 1)]
+  }`;
+};
+
 for (let i = 0; i < 10; ++i) {
   people.push({
-    name: faker.name.findName(),
-    street: faker.address.streetAddress(),
-    city: faker.address.city(),
-    state: faker.address.state(),
-    country: faker.address.country(),
-    telephone: faker.phone.phoneNumberFormat(),
-    birthday: faker.date
-      .between('1990-01-01', '2021-12-31')
-      .toLocaleDateString('en-IN')
-      .split('T')[0],
+    name: getRandomName(),
+    street: getRandomStreet(),
+    city: getRandomCity(),
+    country: getRandomCountry(),
+    telephone: getRandomTelephone(),
+    birthday: getRandomBirthday(),
   });
 }
 
@@ -48,11 +206,26 @@ document.querySelectorAll('.summary_item').forEach((s, i) => {
 });
 
 document.querySelector('.expand').addEventListener('click', (e) => {
-  if (e.target.textContent === 'keyboard_arrow_up') {
+  if (e.target.getAttribute('src') === './arrow_up.svg') {
     document.querySelector('#summary').style.transform = 'scale(1)';
-    e.target.textContent = 'keyboard_arrow_down';
+    e.target.setAttribute('src', './arrow_down.svg');
   } else {
     document.querySelector('#summary').style.transform = 'scale(0)';
-    e.target.textContent = 'keyboard_arrow_up';
+    e.target.setAttribute('src', './arrow_up.svg');
+  }
+});
+
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('../../sw.js', { scope: './' })
+      .then(function () {
+        console.log('ServiceWorker succesfully registered');
+      })
+      .catch(function (err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  } else {
+    console.log('Service workers are not supported.');
   }
 });
