b9b93f83aaa429c1068a98f47cd4d64bc0c706f7
Kamran Ansari <crossthinking786@gmail.com>
2023-02-23 10:42:53 +0530

    [password-generator] show copy button message according to the tab selection

--- intermediate/password-generator/src/components/Main/ButtonLayout/CopyPasswordButton/index.tsx
+++ intermediate/password-generator/src/components/Main/ButtonLayout/CopyPasswordButton/index.tsx
@@ -1,21 +1,23 @@
 import CopySVG from '../../../../assets/CopySVG';
 import Button from '../Button';
 import useOutputStore from '../../../../store/useOutputStore';
+import useParameterStore from '../../../../store/useParameterTypeStore';
 import { toast, Toaster } from 'react-hot-toast';
 import Styles from './index.module.css';
 
 export default function CopyPasswordButton() {
   const outputState = useOutputStore((state) => state.outputValue);
+  const passwordType = useParameterStore(state => state.type);
 
   const handleClick = () => {
     navigator.clipboard.writeText(outputState);
-    toast.success('Password Copied');
+    toast.success(`${passwordType === 'passphrase' ? "Passphrase" : "Password"} Copied`);
   };
 
   return (
     <>
-      <Button title="Copy Password" onClick={handleClick}>
-        {CopySVG}Copy Password
+      <Button title={`Copy ${passwordType === 'passphrase' ? "Passphrase" : "Password"}`} onClick={handleClick}>
+        {CopySVG}Copy {`${passwordType === 'passphrase' ? "Passphrase" : "Password"}`}
       </Button>
       <Toaster
         toastOptions={{
